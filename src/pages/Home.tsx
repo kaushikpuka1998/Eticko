23fb3cd30e597c2bc4c7659ee59012568f113bbd
kaushikpuka1998 <kaushikghosh199832@gmail.com>
2025-07-16 02:40:01 +0530

    Refractoring

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,38 +12,38 @@ import PromisePortion from "../components/PromisePortion/PromisePortion";
 
 const Home = () => {
     return (
-        <body>
-        <div>
-            <hr className="border-t border-gray-300 my-4"/>
-            <div className="flex">
-                <LeftColumnBar/>
-                <Divider/>
-                <div className="flex-1">
-                    <BannerCarousel
-                        images={[
-                            "src/assets/images/frame1.jpg",
-                            "src/assets/images/Frame2.jpg",
-                            "src/assets/images/Frame3.png",
-                            "src/assets/images/Frame4.jpg",
-                        ]}
-                    />
+        <>
+            <div>
+                <hr className="border-t border-gray-300 my-4"/>
+                <div className="flex">
+                    <LeftColumnBar/>
+                    <Divider/>
+                    <div className="flex-1">
+                        <BannerCarousel
+                            images={[
+                                "src/assets/images/frame1.jpg",
+                                "src/assets/images/Frame2.jpg",
+                                "src/assets/images/Frame3.png",
+                                "src/assets/images/Frame4.jpg",
+                            ]}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-        <hr className="border-t border-gray-300 my-4"/>
-        <FlashSalesPortion/>
-        <hr className="border-t border-gray-300 my-4"/>
-        <BestSellingProductsPortion/>
-        <hr className="border-t border-gray-300 my-4"/>
-        <Categoryortion/>
-        <hr className="border-t border-gray-300 my-4"/>
-        <SalePromotionBannerortion/>
-        <hr className="border-t border-gray-300 my-4"/>
-        <ExploreProductPortion/>
-        <hr className="border-t border-gray-300 my-4"/>
-        <NewArrivalPortion/>
-        <PromisePortion/>
-        </body>
+            <hr className="border-t border-gray-300 my-4"/>
+            <FlashSalesPortion/>
+            <hr className="border-t border-gray-300 my-4"/>
+            <BestSellingProductsPortion/>
+            <hr className="border-t border-gray-300 my-4"/>
+            <Categoryortion/>
+            <hr className="border-t border-gray-300 my-4"/>
+            <SalePromotionBannerortion/>
+            <hr className="border-t border-gray-300 my-4"/>
+            <ExploreProductPortion/>
+            <hr className="border-t border-gray-300 my-4"/>
+            <NewArrivalPortion/>
+            <PromisePortion/>
+        </>
     );
 };
 
