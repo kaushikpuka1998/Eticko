34eff192b063b2717688cd76f4845ceb2d1c18bd
kauhsikpuka1998 <kaushikghosh199832@gmail.com>
2025-07-30 21:37:48 +0530

    Backend added with Shell script to run by one command

---
+++ eticko-backend/src/main/java/com/kgstrivers/eticko_backend/EtickoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kgstrivers.eticko_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtickoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtickoBackendApplication.class, args);
+	}
+
+}
